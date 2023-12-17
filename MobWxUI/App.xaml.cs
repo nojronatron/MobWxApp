67571c16c12eb47d61fd8197b4f25a61a6387cce
Jon Rumsey <jon.rumsey@gmail.com>
2023-12-16 16:07:01 -0800

    Set window size, min for Win10 builds only.

--- MobWxUI/App.xaml.cs
+++ MobWxUI/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿namespace MobWxUI
+﻿
+namespace MobWxUI
 {
     public partial class App : Application
     {
@@ -7,6 +8,21 @@
             InitializeComponent();
 
             MainPage = new AppShell();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+#if WINDOWS
+            window.X = 200;
+            window.Y = 300;
+            window.Width = 500;
+            window.Height = 800;
+            window.MinimumWidth = 500;
+            window.MinimumHeight = 800;
+#endif
+            return window;
         }
     }
 }
