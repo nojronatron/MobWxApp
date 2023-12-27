b6265039edab10e08cf5e700778c0100c1a68209
Jon Rumsey <jon.rumsey@gmail.com>
2023-12-27 13:29:20 -0800

    Removed Shell Routing.

--- MobWxUI/AppShell.xaml.cs
+++ MobWxUI/AppShell.xaml.cs
@@ -7,8 +7,11 @@ namespace MobWxUI
         public AppShell()
         {
             InitializeComponent();
-            Routing.RegisterRoute(nameof(CurrentForecastPageView), typeof(CurrentForecastPageView));
-            Routing.RegisterRoute(nameof(SetLocationPageView), typeof(SetLocationPageView));
+            //Routing.RegisterRoute(nameof(MainPage), typeof(MainPage));
+            //Routing.RegisterRoute(nameof(SetLocationPageView), typeof(SetLocationPageView));
+            //Routing.RegisterRoute(nameof(CurrentForecastPageView), typeof(CurrentForecastPageView));
+            //Routing.RegisterRoute(nameof(DetailedConditionsView), typeof(DetailedConditionsView));
+            //Routing.RegisterRoute(nameof(AboutPageView), typeof(AboutPageView));
         }
     }
 }
