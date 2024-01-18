b6d9b8ed32fb6811a820457341e4c70aa3b71d33
Jon Rumsey <jon.rumsey@gmail.com>
2024-01-17 16:35:46 -0800

    Tweak styles for nav, expected usage.

--- MobWxUI/ViewModels/ForecastViewModel.cs
+++ MobWxUI/ViewModels/ForecastViewModel.cs
@@ -2,5 +2,18 @@
 {
     public class ForecastViewModel : BaseViewModel
     {
+        private string _forecastLocation = "Location Placeholder";
+
+        public string ForecastLocation
+        {
+            get => _forecastLocation;
+            set => SetProperty(ref _forecastLocation, value);
+        }
+
+        // todo: show a sub-classed view of 14 forecast data days (bootstrap card-style)
+        public ForecastViewModel()
+        {
+
+        }
     }
 }
