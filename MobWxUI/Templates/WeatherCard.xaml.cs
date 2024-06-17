387a680d74d0bc61b03baee46be62048e7f73bbb
Jon Rumsey <jonrumsey.dev@gmail.com>
2024-06-16 17:43:46 -0700

    Add WxImage fetch to Forecast Cards.

--- MobWxUI/Templates/WeatherCard.xaml.cs
+++ MobWxUI/Templates/WeatherCard.xaml.cs
@@ -1,76 +1,81 @@
-using System.Diagnostics;
-
 namespace MobWxUI.Templates;
 
 public partial class WeatherCard : ContentView
 {
-	public static readonly BindableProperty NameProperty = 
+	public static readonly BindableProperty NameProperty =
 		BindableProperty.Create(nameof(Name), typeof(string), typeof(WeatherCard), default(string), BindingMode.OneWay);
 	public string Name
 	{
-        get => (string)GetValue(NameProperty);
-        set => SetValue(NameProperty, value);
-    }
+		get => (string)GetValue(NameProperty);
+		set => SetValue(NameProperty, value);
+	}
 
 	public static readonly BindableProperty ShortForecastProperty =
 		BindableProperty.Create(nameof(ShortForecast), typeof(string), typeof(WeatherCard), default(string), BindingMode.OneWay);
 	public string ShortForecast
 	{
-        get => (string)GetValue(ShortForecastProperty);
-        set => SetValue(ShortForecastProperty, value);
-    }
+		get => (string)GetValue(ShortForecastProperty);
+		set => SetValue(ShortForecastProperty, value);
+	}
 
 	public static readonly BindableProperty TempProperty =
 		BindableProperty.Create(nameof(Temp), typeof(string), typeof(WeatherCard), default(string), BindingMode.OneWay);
 	public string Temp
 	{
-        get => (string)GetValue(TempProperty);
-        set => SetValue(TempProperty, value);
-    }
+		get => (string)GetValue(TempProperty);
+		set => SetValue(TempProperty, value);
+	}
 
 	public static readonly BindableProperty ProbabilityOfPrecipitationProperty =
 		BindableProperty.Create(nameof(ProbabilityOfPrecipitation), typeof(string), typeof(WeatherCard), default(string), BindingMode.OneWay);
 	public string ProbabilityOfPrecipitation
 	{
-        get => (string)GetValue(ProbabilityOfPrecipitationProperty);
-        set => SetValue(ProbabilityOfPrecipitationProperty, value);
-    }
+		get => (string)GetValue(ProbabilityOfPrecipitationProperty);
+		set => SetValue(ProbabilityOfPrecipitationProperty, value);
+	}
 
 	public static readonly BindableProperty DewpointProperty =
 		BindableProperty.Create(nameof(Dewpoint), typeof(string), typeof(WeatherCard), default(string), BindingMode.OneWay);
 	public string Dewpoint
 	{
-        get => (string)GetValue(DewpointProperty);
-        set => SetValue(DewpointProperty, value);
-    }
+		get => (string)GetValue(DewpointProperty);
+		set => SetValue(DewpointProperty, value);
+	}
 
 	public static readonly BindableProperty RelativeHumidityProperty =
-        BindableProperty.Create(nameof(RelativeHumidity), typeof(string), typeof(WeatherCard), default(string), BindingMode.OneWay);
+		BindableProperty.Create(nameof(RelativeHumidity), typeof(string), typeof(WeatherCard), default(string), BindingMode.OneWay);
 	public string RelativeHumidity
 	{
-        get => (string)GetValue(RelativeHumidityProperty);
-        set => SetValue(RelativeHumidityProperty, value);
-    }
+		get => (string)GetValue(RelativeHumidityProperty);
+		set => SetValue(RelativeHumidityProperty, value);
+	}
 
 	public static readonly BindableProperty WindsProperty =
 		BindableProperty.Create(nameof(Winds), typeof(string), typeof(WeatherCard), default(string), BindingMode.OneWay);
 	public string Winds
 	{
-        get => (string)GetValue(WindsProperty);
-        set => SetValue(WindsProperty, value);
-    }
-
-	public static readonly BindableProperty IconProperty =
-		BindableProperty.Create(nameof(Icon), typeof(string), typeof(WeatherCard), default(string), BindingMode.OneWay);
-	public string Icon
-	{
-		get => (string)GetValue(IconProperty);
-		set =>SetValue(IconProperty, value);
+		get => (string)GetValue(WindsProperty);
+		set => SetValue(WindsProperty, value);
 	}
 
-    public WeatherCard()
+    public static readonly BindableProperty IconProperty =
+        BindableProperty.Create(nameof(Icon), typeof(string), typeof(WeatherCard), default(string), BindingMode.OneWay);
+    public string Icon
+    {
+        get => (string)GetValue(IconProperty);
+        set => SetValue(IconProperty, value);
+    }
+
+	public static readonly BindableProperty WxImageByteArrayProperty =
+		BindableProperty.Create(nameof(WxImageBtyeArray), typeof(byte[]), typeof(WeatherCard), default(byte[]), BindingMode.OneWay);
+	public byte[] WxImageBtyeArray
+	{
+		get => (byte[])GetValue(WxImageByteArrayProperty);
+		set => SetValue(WxImageByteArrayProperty, value);
+	}
+
+	public WeatherCard()
 	{
 		InitializeComponent();
-        Debug.WriteLine("WeatherCard Initialized - BindableProperties now exist.");
 	}
 }
